b6a81b8d5b88a736ffe8f9441775dad162d3edd4
Wallace-Bezerra <89711908+Wallace-Bezerra@users.noreply.github.com>
2022-11-20 01:09:42 -0300

    fix-mobileResponsive

--- src/components/icon/Icon.jsx
+++ src/components/icon/Icon.jsx
@@ -1,6 +1,6 @@
 import style from "./Icon.module.css";
 
-function Icon({ link, iconName, size } ) {
+function Icon({ link, iconName, size }) {
   return (
     <a className={style.link} href={link} target="_blank">
       <img src={`/icons/${iconName}.svg`} alt="" width={size} />
